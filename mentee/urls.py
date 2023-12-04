5c3dcc1abf52089a52ebbd8813c9cf67740afe72
Erlangga Ahmad Khadafi <erlangga.ahmad@ui.ac.id>
2023-12-04 18:19:54 +0700

    add bukti pembayaran implementation

--- mentee/urls.py
+++ mentee/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import show_all_kelas, mentee_home, join_kelas
+from .views import show_all_kelas, mentee_home, join_kelas, add_bukti_pembayaran,\
+    show_form_pendaftaran, get_form_by_id
 
 app_name = 'mentee'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', mentee_home, name='mentee-home'),
     path('kelas-tersedia/', show_all_kelas, name='show_all_kelas'),
     path('join/', join_kelas, name='join_kelas'),
+    path('form-pendaftaran/', show_form_pendaftaran, name='form_pendaftaran'),
+    path('form-pendaftaran/<str:idForm>', get_form_by_id, name='form_by_id'),
+    path('form-pendaftaran/<str:idForm>/add-bukti', add_bukti_pembayaran, name='add_bukti'),
 ]
--- mentee/views.py
+++ mentee/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
-from django.shortcuts import render
-
-from authuser.models import User
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
+from django.shortcuts import redirect, render
+from django.core.serializers import serialize
 from mentee.models import Mentee
 from .decorators import mentee_required
 import kelas.views as kelas_func
+from kelas.forms import PembayaranForm
 
 @mentee_required(login_url='authuser:login')
 def mentee_home(request):
@@ -14,7 +14,19 @@ def mentee_home(request):
 def show_all_kelas(request):
     classes = kelas_func.all_kelas()
 
-    return render(request, 'show_all_kelas.html', {'semua_kelas': classes})
+    return render(request, 'show-all-kelas.html', {'semua_kelas': classes})
+
+@mentee_required(login_url='authuser:login')
+def show_form_pendaftaran(request):
+    forms = kelas_func.filter_form_by_mentee(request.user.id)
+
+    return render(request, 'show-form-pembayaran.html', {'forms': forms})
+
+@mentee_required(login_url='authuser:login')
+def show_form_pendaftaran_by_formId(request):
+    forms = kelas_func.filter_form_by_mentee(request.user.id)
+
+    return render(request, 'show-form-pembayaran.html', {'forms': forms})
 
 @mentee_required(login_url='authuser:login')
 def join_kelas(request):
@@ -33,4 +45,34 @@ def join_kelas(request):
 
     response = kelas_func.join_kelas(request, mentee, kelas)
 
+    return response
+
+@mentee_required(login_url='authuser:login')
+def add_bukti_pembayaran(request, idForm):
+    form_pendaftaran = kelas_func.filter_form_by_id(idForm)
+    if form_pendaftaran is None:
+        return HttpResponseNotFound('Form tidak ditemukan') 
+    
+    if request.method == 'POST':
+        form = PembayaranForm(request.POST, user=request.user)
+        if form.is_valid():
+            form.clean()
+            print(form.cleaned_data.get('url_bukti_pembayaran'))
+            form_pendaftaran.url_bukti_pembayaran = form.cleaned_data.get('url_bukti_pembayaran')
+            form_pendaftaran.status_pembayaran = 'Menunggu Konfirmasi'
+            form_pendaftaran.save()
+            return redirect(f'mentee:form_by_id', idForm=idForm)
+    else:
+        form = PembayaranForm()
+
+    return render(request, 'add-bukti-pembayaran.html', {'form': form, 'idForm': idForm}) 
+
+def get_form_by_id(request, idForm):
+    form = kelas_func.filter_form_by_id(idForm)
+    if form is None:
+        return HttpResponse(status=400, content=f'Form dengan id {idForm} tidak ditemukan')
+    
+    serialized_form = serialize('json', [form])
+    response = JsonResponse(serialized_form, safe=False)
+    
     return response
