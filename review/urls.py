173fdcacf36895f687b290af2552deb72ad61634
hizkiagtg <hizkia.sebastian@ui.ac.id>
2023-12-11 15:47:07 +0700

    Untuk review kelas hampir done

--- review/urls.py
+++ review/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from review.views import review_kelas
+from review.views import review_kelas, classes_followed
 
 app_name = 'review'
 
 urlpatterns = [
+    path('', classes_followed, name='classes_followed'),
     path('review_kelas/', review_kelas, name='review_kelas'),
 ]
--- review/views.py
+++ review/views.py
@@ -6,6 +6,12 @@ from django.contrib import messages
 from review.decorators import mentee_required
 
 @mentee_required(login_url='authuser:login')
+def classes_followed(request):
+    mentee_id = request.user.id
+    kelas_diikuti = Kelas.objects.filter(mentee_kelas=mentee_id)
+    return render(request, 'review_kelas.html', {'kelas_diikuti': kelas_diikuti})
+
+@mentee_required(login_url='authuser:login')
 def review_kelas(request):
     mentee = request.user.id
     reviews = Review.objects.filter(reviewer_id=mentee)
@@ -28,3 +34,9 @@ def review_kelas(request):
 
     return render(request, 'review_kelas.html', 
                   {'reviews': reviews, 'form': form, 'kelas_diikuti': kelas_diikuti})
+
+def review_modal(request, kelas_id):
+    kelas = get_object_or_404(Kelas, pk=kelas_id)
+    form = ReviewForm()
+
+    return render(request, 'review_modal.html', {'form': form, 'kelas': kelas})
