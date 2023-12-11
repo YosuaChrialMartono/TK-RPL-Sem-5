fdab7297e738b07a6704d65f70f9259178b68beb
hizkiagtg <hizkia.sebastian@ui.ac.id>
2023-12-11 20:07:16 +0700

    Admin selesai

--- adminreview/models.py
+++ adminreview/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from mentor.models import Mentor
+from kelas.models import Kelas
 
 class Materi(models.Model):
     judul_materi = models.CharField(max_length=200)
     deskripsi = models.TextField()
-    mentor = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    kelas = models.ForeignKey(Kelas, on_delete=models.CASCADE, null=True)
     status_persetujuan = models.CharField(max_length=20, default='Menunggu Persetujuan')
     feedback = models.TextField(blank=True, null=True)
 
--- adminreview/urls.py
+++ adminreview/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from adminreview.views import pengelolaan_materi, tinjau_materi
+from adminreview.views import pengelolaan_materi, tinjau_materi, tinjau_kelas, persetujuan_materi
 
 app_name = 'adminreview'
 
 urlpatterns = [
-    path('pengelolaan_materi/', pengelolaan_materi, name='pengelolaan_materi'),
-    path('tinjau_materi/<int:materi_id>/', tinjau_materi, name='tinjau_materi'),
+    path('', pengelolaan_materi, name='pengelolaan_materi'),
+    path('tinjau_materi/<int:kelas_id>/', tinjau_materi, name='tinjau_materi'),
+    path('persetujuan_materi/<int:materi_id>/', persetujuan_materi, name='persetujuan_materi'),
+    path('tinjau_kelas/<int:kelas_id>/', tinjau_kelas, name='tinjau_kelas'),
 ]
